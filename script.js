e9151147ab9cb1e657c7c124e09c8377b9b5ade1
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-11-26 13:17:01 +0300

    01ROBOT

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const price = 100;
-const discont = 10;
-const total = price - (price * discont) / 100;
-console.log(total);
-console.log(total > 90);
-console.log(total === 100);
-console.log(total < 100);
+
+let energy = 10;
+
+const INITIAL_ENERGY = 10;
+const MIN_ENERGY = 0;
+const LOW_ENERGY_THRESHOLD = 3;
+
+// console.log ("Текущий уровень заряда: " + energy);
+
+// console.log(`Текущий уровень заряда: ${energy}`);
+
+while (energy >= MIN_ENERGY) {
+    console.log(`Текущий уровень заряда: ${energy}`);
+
+    if (energy === 0) {
+        console.log("Робот грустит...Нужно подзарядить");
+    } else if (energy <= LOW_ENERGY_THRESHOLD) {
+        console.log("Робот начинает чувствовать усталость");
+    } else {
+        console.log("Робот счастлив и бодрствует");
+    }
+
+    energy --;
+}
