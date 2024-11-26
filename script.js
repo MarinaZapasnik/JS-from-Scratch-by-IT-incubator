f945bc251ed7f30f77a575db3de6ec5ab2d2253c
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-11-26 12:58:46 +0300

    rename repository

---
+++ script.js
@@ -0,0 +1,7 @@
+const price = 100;
+const discont = 10;
+const total = price - (price * discont) / 100;
+console.log(total);
+console.log(total > 90);
+console.log(total === 100);
+console.log(total < 100);
