f52b69f5740a81de58eb8ee89ff81d26f0f23dd1
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-12-03 23:26:04 +0300

    commit

---
+++ script03.js
@@ -0,0 +1,24 @@
+// Объявляем глобальную переменную для хранения золота
+let gold = 100;
+
+// Попытка построить башню
+let buildingName = "Tower";
+let costGold = 30;
+
+// проверяем, хватает ли нам золота для строительства
+if (gold >= costGold) {
+  // вычитаем золото
+  gold -= costGold; // то же, что gold = gold - costGold
+    console.log(`${buildingName}: work complete!`);
+} else {
+    console.log(`${buildingName}: not enough resources!`);
+}
+
+function createBuilding(buildingName, costGold) {
+    if (gold >= costGold) {
+        gold -= costGold
+        console.log(`${buildingName}: work complete!`)
+    } else {
+        console.log(`${buildingName}: not enough resources!`)
+    }
+}
