7dff1f3a09d65fe1ff7ce4f6c8919225bd1fb27b
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-12-08 01:58:21 +0300

    commit

--- script05.js
+++ script05.js
@@ -5,51 +5,58 @@ const scores = [60, 50, 60, 58, 54, 54,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
 
-    let i = 0;
-    let output;
-    let highScore = -Infinity;
-    let minScore = Infinity; 
-
-    while (i < scores.length) {
-        output = `Bubble solution #${i} score: ${scores[i]}`;
-        console.log(output);
-        i ++
-    }
-
-    console.log(`Bubbles tests: ${scores.length}`);
-
-    for (let i = 0; i < scores.length; i ++) {
-        output = `Bubble solution #${i} score: ${scores[i]}`;
-        console.log(output);
-        if (highScore < scores[i]) {
-            highScore = scores[i]
-        }
-        if (minScore > scores[i]) {
-            minScore = scores[i]
-        }
-    }
     
+    function printScore (array) {
+        let output;
+        for (let i = 0; i < array.length; i ++) {
+            output = `Bubble solution #${i} score: ${array[i]}`;
+            console.log(output);
+        }
+        
+    }
+
+    function getHighScore (array) {
+        let highScore = -Infinity;
+        for (let i = 0; i < array.length; i ++) {
+            if (highScore < array[i]) {
+                highScore = array[i]
+            }
+        }
+        return highScore
+    }
+
+    function getMinScore (array) {
+        let minScore = Infinity; 
+        for (let i = 0; i < array.length; i ++) {
+            if (minScore > array[i]) {
+                minScore = array[i]
+            }
+        }
+        return minScore
+    }
+
+    function getBestScores(array, value) {
+            let bestSolutions = [];
+            for (let i = 0; i < array.length; i ++) {
+                if (array[i] === value) {
+                    bestSolutions.push(array[i]);
+                }
+            
+        }
+        return bestSolutions
+    }
+
+
+    const highScore = getHighScore(scores);
+    const minScore =  getMinScore(scores)
+
     console.log(`Bubbles tests: ${scores.length}`);
+    printScore(scores);
+
     console.log(`Highest bubble score: ${highScore}`);
-
-    const bestSolutions = [];
-    const minSolutions = [];
-
-    for (let i = 0; i < scores.length; i ++) {
-        if (scores[i] === highScore) {
-            bestSolutions.push(scores[i]);
-        }
-        if (scores[i] === minScore) {
-            minSolutions.push(scores[i]);
-        }
-    }
-
-    
-    console.log(`Solutions with highest score: ${bestSolutions}`);
-    console.log(`Solutions with minimal score: ${minSolutions}`);
-
+    console.log(`Minimal bubble score: ${minScore}`);
     
 
     
-
-    
+    console.log(`Solutions with highest score: ${getBestScores(scores, highScore)}`);
+    console.log(`Solutions with minimal score: ${getBestScores(scores, minScore)}`);
