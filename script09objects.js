bf8e29265481ed58c05660015850c2e9efd5b83e
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-12-11 23:48:48 +0300

    commit

---
+++ script09objects.js
@@ -0,0 +1,30 @@
+
+const prop = "name"
+const car = {
+    year: 2015,
+    mark: "BMW",
+    fuel: "diesel",
+    model: "X5",
+    engine: 2.5,
+    [prop]: "beshka"
+}
+
+const human = {
+    gender: "female",
+    [prop]: "Anna",
+    age: 25,
+}
+
+//----конструктор объекта------
+
+class Student {
+    constructor(name, age, isStudent) { ///похоже на функцию, которая принимает параметры
+        this.name = name,
+        this.age = age,
+        this.isStudent = isStudent,
+    }
+}
+
+//---теперь используем этот конструктор для создания объекта
+
+const student = new Student("Bob", 32, true) /// здесь аргументы
